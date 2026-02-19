2c778f0b20532620f270cb0c2345dcc6026a131a
Gino Zampa <ginojesus2002@gmail.com>
2026-02-19 01:29:11 -0300

    correcion de errores enores

--- back-end/src/users/users.module.ts
+++ back-end/src/users/users.module.ts
@@ -3,15 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
-import { LocalitiesModule } from 'src/localities/localities.module';
-import { LocalitiesService } from 'src/localities/localities.service';
 import { Purchase } from 'src/purchases/entities/purchase.entity';
 import { Shipment } from 'src/shipments/entities/shipment.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Purchase, Shipment]), LocalitiesModule],
+  imports: [TypeOrmModule.forFeature([User, Purchase, Shipment])],
   controllers: [UsersController],
-  providers: [UsersService, LocalitiesService],
+  providers: [UsersService],
   exports: [UsersService],
 })
 export class UsersModule { }
